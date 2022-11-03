005f0321845fae16596ea6d690be54561d75297d
SharathPayili <sharathchandravivek@gmail.com>
2022-11-03 22:28:50 +0000

    save routine

--- git_cmd.sh
+++ git_cmd.sh
@@ -3,12 +3,14 @@
 git_command=$1
 
 if ((git_command == "pull")); then
-	git pull origin master
+	git add .
+	git commit -m "save routine"
+	git pull origin main
 	# if (($2 == "f")); then
 	# 	git pull -f origin main
 	# fi
 elif ((git_command == "push")); then
-	git push origin master
+	git push origin main
 	# if (($2 == "f")); then
 	# 	git push -f origin main
 	# fi
