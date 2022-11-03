a7373900704f47b3672ea3b4d7437151840ffc5a
Sharath <68585549+SharathPayili@users.noreply.github.com>
2022-11-04 03:49:05 +0530

    .

--- git_cmd.sh
+++ git_cmd.sh
@@ -2,16 +2,18 @@
 
 git_command=$1
 
-if ((git_command == "pull")); then
-	git pull origin master
-	if (($2 == "f")); then
-		git pull -f origin main
-	fi
-elif ((git_command == "push")); then
-	git push origin master
-	if (($2 == "f")); then
-		git push -f origin main
-	fi
-else
-	echo "first parameter required."
-fi
+git pull -f origin main
+
+# if ((git_command == "pull")); then
+# 	git pull origin master
+# 	if (($2 == "f")); then
+# 		git pull -f origin main
+# 	fi
+# elif ((git_command == "push")); then
+# 	git push origin master
+# 	if (($2 == "f")); then
+# 		git push -f origin main
+# 	fi
+# else
+# 	echo "first parameter required."
+# fi
