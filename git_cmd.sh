ca26acbc31f74bb78e5ef0946f98fd62f51632e3
Sharath <68585549+SharathPayili@users.noreply.github.com>
2022-11-04 03:47:40 +0530

    updated repo to main in git_cmd

--- git_cmd.sh
+++ git_cmd.sh
@@ -5,12 +5,12 @@ git_command=$1
 if ((git_command == "pull")); then
 	git pull origin master
 	if (($2 == "f")); then
-		git pull -f origin master
+		git pull -f origin main
 	fi
 elif ((git_command == "push")); then
 	git push origin master
 	if (($2 == "f")); then
-		git push -f origin master
+		git push -f origin main
 	fi
 else
 	echo "first parameter required."
